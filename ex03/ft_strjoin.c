75eb04410a03d9e49cf5f04973e9073eff498e0d
Davide Carassiti <dcarassi@e3r8p3.42roma.it>
2022-10-31 17:18:50 +0100

    aaaaa

---
+++ ex03/ft_strjoin.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcarassi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 14:47:28 by dcarassi          #+#    #+#             */
+/*   Updated: 2022/10/31 15:53:59 by dcarassi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	char	*bufus;
+	char	*vettore;
+	int		counter;
+
+	if (size == 0)
+	{
+		*vettore = '\0';
+		return (vettore);
+	}
+	while (strs[counter])
+	{
+	}
+}
